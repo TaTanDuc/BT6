0dcbae338abb35ecd4368313ff41f16d30274b15
Tạ Tân Đức <tatanduc2003@gmail.com>
2023-10-05 10:14:34 +0700

    Add project files.

---
+++ BT66/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace BT66
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 51);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(425, 203);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Mã NV";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Tên NV";
+            this.columnHeader2.Width = 176;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Ngày Sinh";
+            this.columnHeader3.Width = 88;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Phòng Ban";
+            this.columnHeader4.Width = 97;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(198, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(356, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Danh Sách Nhân Viên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(469, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mã NV:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(465, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tên NV:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(453, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Ngày Sinh:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(452, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Phòng Ban:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 260);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Thêm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(139, 260);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Sửa";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(220, 260);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Xóa";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(301, 260);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Thoát";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(518, 200);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(200, 21);
+            this.comboBox2.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(518, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(518, 115);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 20);
+            this.textBox2.TabIndex = 13;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(518, 158);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 301);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ BT66/Form1.cs
@@ -0,0 +1,132 @@
+﻿using BT66.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BT66
+{
+    public partial class Form1 : Form
+    {   
+        NhanVienEntities context = new NhanVienEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void update()
+        {   
+            listView1.Items.Clear();
+            foreach (NV v in context.NVs.ToList())
+            {
+                ListViewItem item = new ListViewItem();
+                item.Text = v.MaNV;
+                item.SubItems.Add(v.TenNV);
+                item.SubItems.Add(v.NgaySinh.ToString());
+                item.SubItems.Add(v.PB.TenPB);
+                listView1.Items.Add(item);
+            }
+        }
+
+        public bool duyetMSNV(string duyet)
+        {
+            foreach(NV nv in context.NVs)
+            {
+                if(nv.MaNV.Equals(duyet))
+                    return true;
+            }
+            return false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            update();
+
+            List<PB> listPB = context.PBs.ToList();
+            comboBox2.DataSource = listPB;
+            comboBox2.DisplayMember = "TenPB";
+            comboBox2.ValueMember = "MaPB";
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+                return;
+
+            textBox1.Text = listView1.SelectedItems[0].Text;
+            textBox2.Text = listView1.SelectedItems[0].SubItems[1].Text;
+            dateTimePicker1.Text = listView1.SelectedItems[0].SubItems[2].Text;
+            comboBox2.Text = listView1.SelectedItems[0].SubItems[3].Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!", "Lỗi!", MessageBoxButtons.OK);
+                return;
+            }
+            if (duyetMSNV(textBox1.Text) == true)
+            {
+                MessageBox.Show("Đã tồn tại MSNV!", "Lỗi!", MessageBoxButtons.OK);
+                return;
+            }
+            
+            NV nv = new NV();
+            nv.MaNV = textBox1.Text;
+            nv.TenNV = textBox2.Text;
+            nv.NgaySinh = dateTimePicker1.Value;
+            nv.MaPB = comboBox2.SelectedValue.ToString();
+
+            context.NVs.Add(nv);
+            context.SaveChanges();
+            update();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            NV nv = context.NVs.FirstOrDefault(p => p.MaNV == textBox1.Text);
+            DialogResult dr = MessageBox.Show("Bạn có muốn xóa!","Cảnh Báo!",MessageBoxButtons.OKCancel);
+            if (nv != null && dr == DialogResult.OK)
+            {
+                context.NVs.Remove(nv);
+                context.SaveChanges();
+                update();
+            }
+            else
+                return;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            NV nv = context.NVs.FirstOrDefault(p => p.MaNV == textBox1.Text);
+            if (nv == null)
+            {
+                MessageBox.Show("Không tồn tại MSNV!", "Lỗi!", MessageBoxButtons.OK);
+                return;
+                
+            }
+            else if(MessageBox.Show("Bạn có muốn sửa!", "Cảnh Báo!", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                nv.TenNV = textBox2.Text;
+                nv.NgaySinh = dateTimePicker1.Value;
+                nv.MaPB = comboBox2.SelectedValue.ToString();
+
+                context.SaveChanges();
+                update();
+            }
+        }
+    }
+}
